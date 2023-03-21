e029ef39e5ab35d42d8291bdfb78b8b8f35c969c
trangnv <nvtrang91@gmail.com>
2023-03-21 16:09:01 +0700

    update checkpoint

--- src/writers.ts
+++ src/writers.ts
@@ -10,26 +10,31 @@ import type { CheckpointWriter } from '@snapshot-labs/checkpoint';
 //
 // See here for the original logic used to create post transactions:
 // https://gist.github.com/perfectmak/417a4dab69243c517654195edf100ef9#file-index-ts
-export async function handleTransfer({ block, tx, event, mysql }: Parameters<CheckpointWriter>[0]) {
+export async function handle_account_initialized({
+  block,
+  tx,
+  event,
+  mysql
+}: Parameters<CheckpointWriter>[0]) {
   if (!event) return;
 
-  const from = toAddress(event.data[0]);
-  const to = toAddress(event.data[1]);
-  const value = BigInt(event.data[2]);
+  const public_key = toAddress(event.data[0]);
+  // const to = toAddress(event.data[1]);
+  // const value = BigInt(event.data[2]);
   const timestamp = block.timestamp;
   const blockNumber = block.block_number;
 
   // transfer object matches fields of Transfer type in schema.gql
-  const transfer = {
+  const new_acc = {
     id: `${tx.transaction_hash}`,
-    from,
-    to,
-    value,
+    public_key,
+    // to,
+    // value,
     tx_hash: tx.transaction_hash,
     created_at: timestamp,
     created_at_block: blockNumber
   };
 
   // table names are `lowercase(TypeName)s` and can be interacted with sql
-  await mysql.queryAsync('INSERT IGNORE INTO transfers SET ?', [transfer]);
+  await mysql.queryAsync('INSERT IGNORE INTO new_accs SET ?', [new_acc]);
 }
