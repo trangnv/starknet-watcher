0112c0e87b487770d05d6327c569e9354e7a9eb0
trangnv <nvtrang91@gmail.com>
2023-04-12 23:34:09 +0700

    toward accounts_dashboard

---
+++ notebooks/utils/query.py
@@ -0,0 +1,45 @@
+import requests
+
+graph_url = 'http://localhost:3000/'
+
+def submit_query(query: str) -> dict:
+    request = requests.post(graph_url, "", json={"query": query})
+    if request.status_code != 200:
+        raise Exception(f"Query failed. Return code is {request.status_code}\n{query}")
+
+    result = request.json()
+
+    return result
+
+def query_account_creates():
+    account_creates = []
+    offset = 0
+    chunk_size = 1000
+    while True:
+        query = """
+          {
+            account_creates(
+                first: %d, 
+                skip: %d,
+                orderBy: created_at_block,
+                orderDirection: asc
+            ) {
+                id
+                from_address
+                created_at
+                created_at_block
+            }
+          }
+          """ % (
+            chunk_size,
+            offset,
+        )
+        result = submit_query(query)
+        account_create_records = result["data"]["account_creates"]
+        if len(account_create_records) == 0:
+            break
+        account_creates+=account_create_records
+
+        offset += chunk_size
+    
+    return account_creates
--- src/writers.ts
+++ src/writers.ts
@@ -1,15 +1,29 @@
 import { hexStrArrToStr, toAddress } from './utils';
 import type { CheckpointWriter } from '@snapshot-labs/checkpoint';
 
-// export async function handleDeploy() {
-//   // Run logic as at the time Contract was deployed.
-// }
+export async function handle_account_created({
+  block,
+  tx,
+  rawEvent,
+  mysql
+}: Parameters<CheckpointWriter>[0]) {
+  if (!rawEvent) return;
 
-// This decodes the new_post events data and stores successfully
-// decoded information in the `posts` table.
-//
-// See here for the original logic used to create post transactions:
-// https://gist.github.com/perfectmak/417a4dab69243c517654195edf100ef9#file-index-ts
+  const from_address = rawEvent.from_address;
+  const timestamp = block.timestamp;
+  const blockNumber = block.block_number;
+
+  const account_create = {
+    id: `${tx.transaction_hash}`,
+    // tx_hash: tx.transaction_hash,
+    from_address: from_address,
+    created_at: timestamp,
+    created_at_block: blockNumber
+  };
+
+  // table names are `lowercase(TypeName)s` and can be interacted with sql
+  await mysql.queryAsync('INSERT IGNORE INTO account_creates SET ?', [account_create]);
+}
 export async function handle_account_upgraded({
   block,
   tx,
@@ -23,15 +37,15 @@ export async function handle_account_upgraded({
   const blockNumber = block.block_number;
 
   // transfer object matches fields of Transfer type in schema.gql
-  const acc_impl = {
+  const account_upgrade = {
     id: `${tx.transaction_hash}`,
-    implementation: implementation,
+    // tx_hash: tx.transaction_hash,
     from_address: from_address,
-    tx_hash: tx.transaction_hash,
     created_at: timestamp,
-    created_at_block: blockNumber
+    created_at_block: blockNumber,
+    implementation: implementation
   };
 
   // table names are `lowercase(TypeName)s` and can be interacted with sql
-  await mysql.queryAsync('INSERT IGNORE INTO acc_impls SET ?', [acc_impl]);
+  await mysql.queryAsync('INSERT IGNORE INTO account_upgrades SET ?', [account_upgrade]);
 }
